284378a1f8de2ec5fbfca27d4449c0fad4ba521f
eysix <andrewau121@gmail.com>
2024-01-08 22:01:17 -0500

    Update about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,21 +4,16 @@ const About = () => {
   return (
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
+      <p>Hi there! 👋<br /> Thanks for checking out Yumful 2.0! </p>
       <p>
-        Too lazy: So here's a ChatGPT generated paragraph:
-      </p>
-      <p>
-        Welcome to Yumful's Deliciously Dark Recipes, where we dive into the
-        deepest, darkest secrets of the culinary underworld (okay, maybe it's
-        just the pantry). Our recipes are so cool that they make ice jealous,
-        and our jokes are so dark that even the oven's afraid to tell them. We
-        don't just share recipes; we share a good laugh while you cook. Our
-        secret ingredient? A pinch of humor, a dash of mischief, and a side of,
-        "Did you hear the one about the talking potato?" So, whether you're a
-        master chef or just a slightly confused microwave operator, you're in
-        for a wickedly tasty and amusing ride. Because at Yumful's Deliciously
-        Dark Recipes, we believe that food should not only taste amazing but
-        crack a few jokes along the way.
+        You'll find plenty of exotic and classic recipes graciously provided by
+        TheMealDB database.
+        <a href="https://www.themealdb.com/" target="_blank">
+          Check them out here!
+        </a>{" "}
+        Yumful 2.0 allows you to search the database for what you want to eat
+        and provides fantastic recipes from all over the web so that you can
+        polish your palette and eat like a king! 👑
       </p>
     </section>
   );
