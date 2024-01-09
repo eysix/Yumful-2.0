1deade70a4aa92941163fe169238fc8b9641a5e8
eysix <andrewau121@gmail.com>
2024-01-08 22:06:45 -0500

    Update About styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,14 +3,18 @@ import React from 'react'
 const About = () => {
   return (
     <section className="section about-section">
-      <h1 className="section-title">about us</h1>
-      <p>Hi there! 👋<br /> Thanks for checking out Yumful 2.0! </p>
+      <h1 className="section-title">about Yumful</h1>
+      <p>
+        Hi there! 👋 Thanks for checking out Yumful 2.0!{" "}
+      </p>
       <p>
         You'll find plenty of exotic and classic recipes graciously provided by
-        TheMealDB database.
+        TheMealDB database.{" "}
         <a href="https://www.themealdb.com/" target="_blank">
           Check them out here!
         </a>{" "}
+      </p>
+      <p>
         Yumful 2.0 allows you to search the database for what you want to eat
         and provides fantastic recipes from all over the web so that you can
         polish your palette and eat like a king! 👑
